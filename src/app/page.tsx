a8ee9b78a987cc581b11529d60b97d062c3aada4
Victor Alves <victorhugo_sp4@hotmail.com>
2024-04-30 21:16:18 -0300

    página inicial agora apenas carrega o componente de lista de notas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,13 @@
 "use client";
 
-import NoteForm from "@/components/NoteForm/noteForm";
+import NoteList from "@/components/NoteList/noteList";
 import { useLocalStorage } from "../../server/useLocalStorage";
-import { useMemo } from "react";
-import { v4 as uuidV4 } from "uuid"
-
-export type RawNote = {
-  id: string
-} & RawNoteData
-
-export type RawNoteData = {
-  title: string
-  markdown: string
-  tagIds: string[]
-}
-
-export type Note = {
-  id: string
-} & NoteData
-
-export type NoteData = {
-  title: string
-  markdown: string
-  tags: Tag[]
-}
-
-export type Tag = {
-  id: string
-  label: string
-}
+import { Tag } from "./new-note/page";
 
 export default function Home() {
-  const [notes, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
   const [tags, setTags] = useLocalStorage<Tag[]>("TAGS", [])
-
-  const notesWithTags = useMemo(() => {
-    return notes.map(note => {
-      return {... notes, tags: tags.filter(tag => note.tagIds.includes(tag.id))}
-    })
-  }, [notes, tags])
-
-  function onCreateNote({tags, ...data}: NoteData){
-    setNotes(prevNotes => {
-      return [...prevNotes, {...data, id: uuidV4(), tagIds: tags.map(tag => tag.id)}]
-    })
-  }
-
-  function AddTag(tag: Tag){
-    setTags(prev => [...prev, tag])
-  }
-
-  return (
-    <main>
-      <NoteForm onSubmit={onCreateNote} onAddTag={AddTag} availableTags={tags} />
-    </main>
-  );
+  
+  return(
+    <NoteList availableTags={tags} />
+  )
 }
