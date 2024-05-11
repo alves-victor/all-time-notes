613fbc4fe4636449b6171b75fe577071e36f8524
Victor Alves <victorhugo_sp4@hotmail.com>
2024-05-11 19:51:39 -0300

    add modal de confirmação e função para exclusão de nota

--- src/components/NoteView/noteView.tsx
+++ src/components/NoteView/noteView.tsx
@@ -1,9 +1,12 @@
-import { Note } from "@/app/new-note/page"
-import { Button } from "flowbite-react"
+import { Note, RawNote } from "@/app/new-note/page"
+import { Button, Modal } from "flowbite-react"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
 import ReactMarkdown from "react-markdown"
 import "./style.css"
+import { useLocalStorage } from "../../../server/useLocalStorage"
+import { useState } from "react"
+import { HiOutlineExclamationCircle } from "react-icons/hi"
 
 type NoteViewProps = {
     notes: Note[]
@@ -11,13 +14,44 @@ type NoteViewProps = {
 }
 
 export default function NoteView({ notes, id }: NoteViewProps){
+    const [notes_, setNotes] = useLocalStorage<RawNote[]>("NOTES", [])
+    const [openModal, setOpenModal] = useState(false)
     const note = notes.find(note => note.id == id)
     const route = useRouter()
 
     if(note == null || note == undefined){ route.push("/") }
 
+    function DeleteNote(){
+        setNotes(prevNotes => {
+            return prevNotes.filter(note => note.id !== id)
+        })
+
+        route.push("/")
+    }
+
     return(
         <div className="py-6 px-6 lg:px-64 md:px-32">
+
+            <Modal dismissible show={openModal} size="md" onClose={() => setOpenModal(false)} popup>
+                <Modal.Header />
+                <Modal.Body>
+                    <div className="text-center">
+                        <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+                        <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+                            Tem certeza que quer deletar esta nota?
+                        </h3>
+                        <div className="flex justify-center gap-4">
+                        <Button color="failure" onClick={() => DeleteNote()}>
+                            Sim, tenho certeza
+                        </Button>
+                        <Button color="gray" onClick={() => setOpenModal(false)}>
+                            Não quero
+                        </Button>
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
+
             <div className="flex w-full flex-col gap-4 mb-4 md:flex-row sm:justify-between">
                 <div className="w-full flex flex-col">
                     <h1 className="text-4xl mb-2">{ note?.title }</h1>
@@ -28,16 +62,16 @@ export default function NoteView({ notes, id }: NoteViewProps){
                     </ul>
                 </div>
 
-                <div className="w-full flex flex-row gap-4 sm:justify-end md:max-w-max">
+                <div className="w-full flex flex-row gap-4 items-center sm:justify-end md:max-w-max">
                     <Link href={{
                         pathname: "/edit-note",
                         query: {"note-id": note?.id}
                     }}>
                         <Button>Editar</Button>
                     </Link>
-                    <Link href={"#"}>
-                        <Button color={"failure"}>Deletar</Button>
-                    </Link>
+                    
+                    <Button className="max-h-min" onClick={() => setOpenModal(true)} color={"failure"}>Excluir</Button>
+                    
                     <Link href={"/"}>
                         <Button color={"gray"}>Voltar</Button>
                     </Link>
