83c21b2c22de879980e38189c05999c6489b642d
Victor Alves <victorhugo_sp4@hotmail.com>
2024-05-11 19:56:16 -0300

    parâmetros tranferidos para o componente NoteList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,10 @@
-"use client";
+"use client"
 
-import NoteList from "@/components/NoteList/noteList";
-import { useLocalStorage } from "../../server/useLocalStorage";
-import { RawNote, Tag } from "./new-note/page";
-import { useMemo } from "react";
+import NoteList from "@/components/NoteList/noteList"
 
 export default function Home() {
-  const [notes] = useLocalStorage<RawNote[]>("NOTES", [])
-  const [tags] = useLocalStorage<Tag[]>("TAGS", [])
-
-  const notesWithTags = useMemo(() => {
-    return notes.map(note => {
-      return {...note, tags: tags.filter(tag => note.tagIds.includes(tag.id))}
-    })
-}, [notes, tags])
-  
+ 
   return(
-    <NoteList notes={notesWithTags} availableTags={tags} />
+    <NoteList />
   )
 }
